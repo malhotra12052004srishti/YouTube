913f92dfe7a65ee1deff0b5dcf3aed63ed95a56d
SRISHTI <Srishtiludhiana@gmail.com>
2025-02-12 11:04:57 +0530

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <h1 className="font-bold text-3xl bg-red-300">Namaste Youtube</h1>
+      </div>
+  )
+}
+
+export default App
