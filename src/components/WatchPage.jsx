086c60fb85240aafb97a6bbfabb023928adfaafc
SRISHTI <Srishtiludhiana@gmail.com>
2025-02-13 17:01:46 +0530

    N-level Comments Added

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [params] = useSearchParams();
@@ -14,17 +15,22 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="p-5 bg-white flex w-full">
-      <iframe
-        width="100%"
-        height="450" 
-        src={`https://www.youtube.com/embed/${params.get("v")}?autoplay=1`}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; encrypted-media; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-        className="shrink-0 max-w-[800px] w-full" 
-      />
+    <div className="flex flex-col">
+      <div className="p-5 bg-white flex w-296">
+        <iframe
+          width="100%"
+          height="450"
+          src={`https://www.youtube.com/embed/${params.get("v")}?autoplay=1`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; encrypted-media; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="shrink-0 max-w-[800px] w-full"
+        />
+      </div>
+      <div>
+        <CommentsContainer />
+      </div>
     </div>
   );
 };
