852d32cbb39423ed0663aea7d230d5fcf7463f05
SRISHTI <Srishtiludhiana@gmail.com>
2025-02-13 09:52:26 +0530

    Debouncing Lecture

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,3 +10,30 @@ const Button = ({ name }) => {
 };
 
 export default Button;
+
+
+
+
+
+
+
+
+
+/**
+ * DEBOUNCING
+ * 
+ * typing slow = 200ms
+ * typing fast = 30ms
+ * 
+ * Performance :
+ *  - iphone pro max = 14 letters * 1000 = 14000
+ *  - with debouncing = 3 API calls * 1000 = 3000
+ * 
+ * Debouncing with 200ms
+ * - if the difference between the 2 key strokes is less than 200ms - DECLINE THE API CALL
+ * - if the difference between the 2 key strokes is more than 200ms - ACCEPT THE API CALL
+ * 
+ * 
+ * FLIPKART IS A GOOD EXAMPLE FOR DEBOUNCING BUT YTUBE IS HAVING VERY LESS DEBOUNCING TECHNIQUES AS IT WANTS TO GIVE BETTER USER EXPERIENCE
+ * 
+ */
